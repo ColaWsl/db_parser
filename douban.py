f881b7f2ca851b173fb9980f65bf8b66555db3c0
Wangsl <wsl612425@163.com>
2023-05-05 16:55:16 +0800

    update

--- douban.py
+++ douban.py
@@ -1,54 +1,9 @@
 import csv
-import random
 import requests
+import random
+from util import agent
+from util import ipproxy
 from bs4 import BeautifulSoup
-
-
-# 随机生成浏览器标识 user agent
-def get_user_agent():
-    first_num = random.randint(55, 76)
-    third_num = random.randint(0, 3800)
-    fourth_num = random.randint(0, 140)
-    os_type = [
-        '(Windows NT 6.1; WOW64)', '(Windows NT 10.0; WOW64)', '(X11; Linux x86_64)',
-        '(Macintosh; Intel Mac OS X 10_14_5)'
-    ]
-    chrome_version = 'Chrome/{}.0.{}.{}'.format(first_num, third_num, fourth_num)
-
-    user_agent = ' '.join(['Mozilla/5.0', random.choice(os_type), 'AppleWebKit/537.36',
-                           '(KHTML, like Gecko)', chrome_version, 'Safari/537.36']
-                          )
-    return user_agent
-
-
-# 获取快代理免费的高密IP
-def get_ipinfo(soup, str_craw, key, value):
-    a = soup.find(str_craw, attrs={key: value}).text
-    return a
-
-
-# 抓取快代理第一页：类型，IP地址，
-ip_info = []
-
-for i in range(2, 3):  # 前1页
-    url = r"https://www.kuaidaili.com/free/inha/{}".format(i)
-    headers = {
-        'User-Agent': get_user_agent()
-    }
-
-    res = requests.get(url=url, headers=headers)
-    soup = BeautifulSoup(res.text, features='lxml')
-
-    link_table = soup.find('tbody')
-
-    for tr_data in link_table.find_all('tr'):
-        dic = {}
-        ip = str(get_ipinfo(tr_data, 'td', 'data-title', 'IP'))
-        http = str(get_ipinfo(tr_data, 'td', 'data-title', '类型'))
-        dic[http] = ip
-        ip_info.append(dic)
-
-print(ip_info)
 
 
 class DoubanParser:
@@ -57,8 +12,8 @@ class DoubanParser:
     def parse(self, page_url):
 
         for num in range(0, 40, 20):
-            headers = {'User-Agent': get_user_agent()}
-            proxy = random.choice(ip_info)
+            headers = {'User-Agent': agent.get()}
+            proxy = random.choice(ipproxy.get_ips())
             url = page_url + '?start=' + num.__str__()
             res = requests.get(url=url, headers=headers, proxies=proxy)
 
@@ -78,8 +33,8 @@ class DoubanParser:
                     break
 
     def get_book_description(self, page_url):
-        headers = {'User-Agent': get_user_agent()}
-        proxy = random.choice(ip_info)
+        headers = {'User-Agent': agent.get()}
+        proxy = random.choice(ipproxy.get_ips())
         res = requests.get(url=page_url, headers=headers, proxies=proxy)
 
         page_soup = BeautifulSoup(res.text, features='lxml')
